0a208105ecb284250493ca9e2960941db6296870
Arys <charles.yaiche@gmail.com>
2012-12-30 18:01:37 +0100

    caméra légerement amélioré
    avec haut et bas
    touches gerer en azerty et qwerty

--- Game v0.2/Camera.cs
+++ Game v0.2/Camera.cs
@@ -22,28 +22,13 @@ namespace MiniCube
     {
         static public void orient_camera(ref Input input, ref Vector3 position, ref Vector3 angle)
         {
-            float[] VitesseRotation = new float[3] { 0.005f, 0.005f, 0.005f };
+            float[] VitesseRotation = new float[3] { .005f, 0.005f, 0.005f };
             float[] VitesseTranslation = new float[3] { 
-                Convert.ToSingle(Math.Sqrt(0.00005f)),
-                Convert.ToSingle(Math.Sqrt(0.00005f)),
-                Convert.ToSingle(Math.Sqrt(0.00005f)),
+                Convert.ToSingle(Math.Sqrt(.001f)),
+                Convert.ToSingle(Math.Sqrt(.001f)),
+                Convert.ToSingle(Math.Sqrt(.001f)),
             };
-            double produit_scalaire,produit_scalaire2;
-
-            /************ Utile pour faire des tests ************/
-            if (input.KeysDown.Contains(Keys.R)) // Reset
-            {
-                angle = new Vector3(0, 0, 0);
-            }
-            if (input.KeysDown.Contains(Keys.NumPad8))
-            {
-                angle.X += VitesseRotation[0];
-            }
-            if (input.KeysDown.Contains(Keys.NumPad2))
-            {
-                angle.X -= VitesseRotation[0];
-            }
-            /************ END ************/
+            double produit_scalaire;
 
             if (input.KeysDown.Contains(Keys.Space))
             {
@@ -57,71 +42,73 @@ namespace MiniCube
                 Console.WriteLine("Vers le bas !");
             }
 
-            if (input.KeysDown.Contains(Keys.Up) || input.KeysDown.Contains(Keys.Z))
+            if (input.KeysDown.Contains(Keys.Up))
             {
                 produit_scalaire = (Math.Cos(angle.Y) * VitesseTranslation[2]);
                 position.Z -= Convert.ToSingle(produit_scalaire);
+
+
                 produit_scalaire = (Math.Sin(angle.Y) * VitesseTranslation[2]);
                 position.X += Convert.ToSingle(produit_scalaire);
+
+
                 Console.WriteLine("En avant !");
             }
-            if (input.KeysDown.Contains(Keys.Down) || input.KeysDown.Contains(Keys.S))
+            if (input.KeysDown.Contains(Keys.Down))
             {
+
                 produit_scalaire = (Math.Cos(angle.Y) * VitesseTranslation[2]);
                 position.Z += Convert.ToSingle(produit_scalaire);
+
+
                 produit_scalaire = (Math.Sin(angle.Y) * VitesseTranslation[2]);
                 position.X -= Convert.ToSingle(produit_scalaire);
                 Console.WriteLine("En arrière !");
             }
 
-            /************ EFFECTUER LES PROJECTIONS ************/
-            if (input.KeysDown.Contains(Keys.Right))
+            if (input.KeysDown.Contains(Keys.D))
             {
                 angle.Y -= VitesseRotation[1];
                 Console.WriteLine("A tribord !");
             }
-            if (input.KeysDown.Contains(Keys.Left))
+
+            if (input.KeysDown.Contains(Keys.A) || input.KeysDown.Contains(Keys.Q))
             {
                 angle.Y += VitesseRotation[1];
                 Console.WriteLine("A babord !");
             }
-            /************ END ************/
-
-            if (input.KeysDown.Contains(Keys.Q))
+            if (input.KeysDown.Contains(Keys.W) || input.KeysDown.Contains(Keys.Z))
             {
+                angle.X += VitesseRotation[1];
+                Console.WriteLine("en haut!");
+            }
+            if (input.KeysDown.Contains(Keys.S))
+            {
+                angle.X -= VitesseRotation[1];
+                Console.WriteLine("en bas!");
+            }
+            if (input.KeysDown.Contains(Keys.Left))
+            {
+                //position.X += VitesseTranslation[0];
+
                 produit_scalaire = (Math.Sin(angle.Y) * VitesseTranslation[0]);
                 position.Z += Convert.ToSingle(produit_scalaire);
+
+
                 produit_scalaire = (Math.Cos(angle.Y) * VitesseTranslation[0]);
                 position.X += Convert.ToSingle(produit_scalaire);
                 Console.WriteLine("Left");
             }
-            if (input.KeysDown.Contains(Keys.D))
+            if (input.KeysDown.Contains(Keys.Right))
             {
                 produit_scalaire = (Math.Sin(angle.Y) * VitesseTranslation[0]);
                 position.Z -= Convert.ToSingle(produit_scalaire);
+
+
                 produit_scalaire = (Math.Cos(angle.Y) * VitesseTranslation[0]);
                 position.X -= Convert.ToSingle(produit_scalaire);
                 Console.WriteLine("Right");
             }
-            if (input.KeysDown.Contains(Keys.PageUp))
-            {
-                produit_scalaire = (Math.Cos(angle.Y)) * VitesseRotation[0];
-                angle.X += Convert.ToSingle(produit_scalaire);
-
-                produit_scalaire = (Math.Sin(angle.Y)) * VitesseRotation[0];
-                angle.Z += Convert.ToSingle(produit_scalaire);
-                Console.WriteLine("Montez !");
-            }
-            if (input.KeysDown.Contains(Keys.PageDown))
-            {
-                produit_scalaire = (Math.Cos(angle.Y)) * VitesseRotation[0];
-                angle.X -= Convert.ToSingle(produit_scalaire);
-
-                produit_scalaire = (Math.Sin(angle.Y)) * VitesseRotation[0];
-                angle.Z -= Convert.ToSingle(produit_scalaire);
-                Console.WriteLine("Coulez !");
-            }
-
             angle.X = Convert.ToSingle(angle.X % (Math.PI * 2));
             angle.Y = Convert.ToSingle(angle.Y % (Math.PI * 2));
             angle.Z = Convert.ToSingle(angle.Z % (Math.PI * 2));
