36132fb8b3329655390d830652221d994670f703
Stin <bast0405free.fr>
2013-06-20 12:47:35 +0200

    Nouveau Obj parser et debut  dun animation reader
    ces deux classe ne sont pas terminer donc inutile d essayer de voire se
    quelles font

---
+++ Game v3.0/Animation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Underground
+{
+    class Animation
+    {
+        //Cette Liste stocke toutes les objets composants une animation. 
+        List<List<Obj>> Anim;
+
+        //La on charge notre liste avec les differents Objet
+        public Animation(List<string> Pathlist)
+        {
+            for (int i = 0; i <= Pathlist.Count; i++ )
+            {
+                Anim.Add(Obj.Obj_parser(Pathlist[i]));
+            }
+        }
+    }
+}
